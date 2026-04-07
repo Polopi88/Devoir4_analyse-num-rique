d494cb111fcda2533543316a593741ab4df6caf0
Edouard Laroche <edlar14@ulaval.ca>
2026-04-07 14:17:19 -0400

    partie a)

--- problimite.py
+++ problimite.py
@@ -1,1 +1,23 @@
-#
+import numpy as np
+from tridiagonal import tridiagonal
+
+def problimite(N, Q, R, a, b_borne, alpha, beta):
+    h = (b_borne - a) / (N + 1)
+    
+    D = -2.0 - Q * (h**2)
+    I = np.ones(N - 1)
+    S = np.ones(N - 1)
+    
+    B = R * (h**2)
+    
+    B[0] = B[0] - alpha
+    B[-1] = B[-1] - beta
+    
+    y_int = tridiagonal(N, D, I, S, B)
+    
+    y_complet = np.zeros(N + 2)
+    y_complet[0] = alpha
+    y_complet[-1] = beta
+    y_complet[1:N+1] = y_int
+    
+    return y_complet
--- tridiagonal.py
+++ tridiagonal.py
@@ -1,1 +1,18 @@
-#
+import numpy as np
+
+def tridiagonal(N, D, I, S, b):
+    D_algo = np.copy(D)
+    b_algo = np.copy(b)
+    x = np.zeros(N)
+    
+    for i in range(1, N):
+        multiplicateur = I[i-1] / D_algo[i-1]
+        D_algo[i] = D_algo[i] - multiplicateur * S[i-1]
+        b_algo[i] = b_algo[i] - multiplicateur * b_algo[i-1]
+        
+    x[-1] = b_algo[-1] / D_algo[-1]
+    
+    for i in range(N-2, -1, -1):
+        x[i] = (b_algo[i] - S[i] * x[i+1]) / D_algo[i]
+        
+    return x
